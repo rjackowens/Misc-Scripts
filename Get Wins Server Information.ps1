1cc9fd9fce542443b3c1d2ca7d9cbdb862ee2441
Jack Owens <jowens@Hunter.com>
2019-01-24 17:08:08 -0600

    Create Get Wins Server Information.ps1

---
+++ Get Wins Server Information.ps1
@@ -0,0 +1,7 @@
+﻿# This script gets the WINS primary and secondary server for all machines on the domain
+
+Get-ADComputer -Filter * -Properties * | Select-Object Name, OperatingSystem -Unique |
+ForEach-Object {
+    $wins = Get-WmiObject win32_networkadapterconfiguration -ComputerName $_.Name | Where-Object IPEnabled -eq $true | Select-Object WINSPrimaryServer, WINSSecondaryServer | Out-String 
+    Write-Host ($_.Name) ($wins)
+}
