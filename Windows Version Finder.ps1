c36b4753a7d03195d405440ed8bb62a7ffe3c444
Jack Owens <jowens@hunter.com>
2019-04-25 11:38:01 -0500

    Create Windows Version Finder.ps1

---
+++ Windows Version Finder.ps1
@@ -0,0 +1,19 @@
+﻿$ServerList = Get-Content -Path "C:\ServerList.txt"
+
+$ExcelObjects = @()
+
+$ServerList | ForEach-Object {
+
+    $ComputerName = Invoke-Command -ComputerName $_ -ScriptBlock {$env:COMPUTERNAME}
+    $Win32 = Get-WmiObject -Computer $_ -Class Win32_OperatingSystem -ErrorAction Continue
+
+    $ExcelObjects += [PSCustomObject] @{
+
+        Server = $ComputerName.PSComputerName
+        Version = $Win32.Version
+        Build = $Win32.BuildNumber
+           
+    }
+}
+
+$ExcelObjects | Export-Csv -Path "C:\VersionList.csv"
