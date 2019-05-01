5cb1cc76c30a9051ad3f0400ee2b7212755c32d0
Jack Owens <jowens@hunter.com>
2019-05-01 15:11:10 -0500

    Created Create Environment Variables.ps1

---
+++ Create Environment Variable.ps1
@@ -0,0 +1,13 @@
+﻿# Default Scope is Machine; change to User or Process with "Type" Parameter
+function New-EnvironmentVariable {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$Name,
+        [Parameter(Mandatory=$true)]
+        [string]$Value,
+        [string]$Type = "Machine"
+        )
+    [Environment]::SetEnvironmentVariable($Name, $Value, $Type)
+    }
+
+New-EnvironmentVariable -Name NewSystemVar -Value Test
