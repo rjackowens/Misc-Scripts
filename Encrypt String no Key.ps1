15be65b5872c8c9ed5ee8190c52e8fd715e2f74f
Jack Owens <jowens@hunter.com>
2019-05-17 11:43:38 -0500

    Created simple encryption scripts

---
+++ Encrypt String no Key.ps1
@@ -0,0 +1,23 @@
+﻿<#
+.SYNOPSIS
+    Creates DPAPI-encrypted system environment variable from plain text string.
+    Use ProtectedData.Unprotect to decrypt data in C# Program.
+#>
+
+$SecureString = Read-Host "Enter Password" -AsSecureString
+
+# Converts secure string to DPAPI Encrypted String
+$EncryptedPassword = ConvertFrom-SecureString -SecureString $SecureString 
+
+function New-EnvironmentVariable {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$Name,
+        [string]$Value,
+        [string]$Scope = "Machine"
+        )
+    [Environment]::SetEnvironmentVariable($Name, $Value, $Scope)
+    }
+
+$EnvVarName = Read-Host "Enter Environment Variable Name"
+New-EnvironmentVariable -Name $EnvVarName -Value $EncryptedPassword
---
+++ Encrypt String with Key.ps1
@@ -0,0 +1,37 @@
+﻿<#
+.SYNOPSIS
+    Creates AES-encrypted system environment variable from plain text string.
+    Writes decryption key to console. 
+#>
+
+#Create Random Decryption Key
+Function Create-Key {
+     $RKey = @()
+     For ($i=1; $i -le 16; $i++) {
+        [Byte]$RByte = Get-Random -Minimum 0 -Maximum 256
+        $RKey += $RByte
+     }
+     $RKey
+}
+
+$Key = Create-Key
+
+$SecureString = Read-Host "Enter Password" -AsSecureString
+
+# Converts secure string to AES Encrypted String
+$EncryptedPassword = ConvertFrom-SecureString -SecureString $SecureString -Key $Key
+
+function New-EnvironmentVariable {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$Name,
+        [string]$Value,
+        [string]$Scope = "Machine"
+        )
+    [Environment]::SetEnvironmentVariable($Name, $Value, $Scope)
+    }
+
+$EnvVarName = Read-Host "Enter Environment Variable Name"
+New-EnvironmentVariable -Name $EnvVarName -Value $EncryptedPassword
+
+Write-Host "Decryption Key:" $Key -NoNewline `n
