9912229b08d4dd04adf89afa4ba7f3b85a1b33e6
Jack Owens <jowens@Hunter.com>
2019-06-06 09:00:13 -0500

    Create Set ETag HTTP Header.ps1

---
+++ Set ETag HTTP Header.ps1
@@ -0,0 +1,7 @@
+﻿Remove-WebConfigurationProperty -PSPath "MACHINE/WEBROOT/APPHOST/Default Web Site/" -Name . -Filter system.webServer/httpProtocol/customHeaders -AtElement @{name='ETag'}
+
+$MD5 = Get-FileHash "C:\inetpub\file.js" -Algorithm MD5
+
+Write-Host "Setting ETag header to" $MD5.hash
+
+Add-WebConfigurationProperty -PSPath "MACHINE/WEBROOT/APPHOST/Default Web Site/" -Name . -Filter system.webServer/httpProtocol/customHeaders -AtElement @{name="ETag";value=$MD5.Hash}
