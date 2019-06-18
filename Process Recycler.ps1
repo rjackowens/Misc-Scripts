d8ff32f5c05bc035a730645c834a0aa0c9a2fc93
Jack Owens <jowens@hunter.com>
2019-06-18 08:56:28 -0500

    Create Process Recycler.ps1

---
+++ Process Recycler.ps1
@@ -0,0 +1,25 @@
+﻿Function Run-Notepad {
+
+    $App = Start-Process notepad.exe -PassThru
+
+    }
+    
+
+Function Kill-Notepad {
+
+    $AppID = Get-Process $App.Name | Select-Object -ExpandProperty id
+    $wshell = New-Object -ComObject wscript.shell
+    $wshell.AppActivate($AppID)
+    $wshell.SendKeys('%{F4}')
+
+    }
+
+while ($true) {
+
+    Run-Notepad
+    Start-Sleep -Seconds 20
+    Kill-Notepad
+    Start-Sleep -Seconds 5
+
+    }
+
