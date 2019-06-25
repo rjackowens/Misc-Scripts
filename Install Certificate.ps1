ffce7f2d8bd3b0e4f7fa80240747e0f1155050b7
Jack Owens <jowens@Hunter.com>
2019-06-25 09:18:28 -0500

    Added troubleshooting scripts

---
+++ Install Certificate.ps1
@@ -0,0 +1,7 @@
+﻿$CertPassword = $env:CertPassword
+$Directory = $env:SYSTEM_DEFAULTWORKINGDIRECTORY
+
+$FilePath = [IO.Path]::Combine($Directory, "_Daemon", "drop", "cert-file.pfx" )
+
+$CertPasswordSecure = $CertPassword | ConvertTo-SecureString -AsPlainText -Force
+Import-PfxCertificate -FilePath $FilePath -CertStoreLocation Cert:\LocalMachine\Root -Password $CertPasswordSecure
---
+++ Test SQL Connection String.ps1
@@ -0,0 +1,6 @@
+﻿$connectionString = "Data Source=SERVER;Initial Catalog=TEST;Integrated Security=true"
+
+$connection = New-Object System.Data.SqlClient.SqlConnection
+$connection.ConnectionString = $connectionString
+$connection.Open()
+$connection.Close()
